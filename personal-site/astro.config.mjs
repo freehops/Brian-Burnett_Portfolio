1b23c0c3ad673ee8be79c5e1096eefe69472be75
Brian A Burnett <bbrianmiraleste@gmail.com>
2025-03-03 20:07:26 -0800

    Update astro.config.mjs

--- personal-site/astro.config.mjs
+++ personal-site/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fereehops.github.io',
+    site: 'https://freehops.github.io',
     base: 'Brian-Burnett_Portfolio/personal-site'
 
 })
