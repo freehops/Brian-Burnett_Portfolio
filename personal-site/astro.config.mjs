e8996c558edffe22aa3b314543524bff73940f80
Brian A Burnett <bbrianmiraleste@gmail.com>
2025-03-03 20:06:37 -0800

    Github Pages Try 2

--- personal-site/astro.config.mjs
+++ personal-site/astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
     site: 'https://fereehops.github.io',
-    base: 'personal-site'
+    base: 'Brian-Burnett_Portfolio/personal-site'
 
 })
